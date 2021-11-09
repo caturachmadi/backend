71dd20f6da8dc250e4ba58d60e758ea8a851d745
Catur Achmadi <68488619+caturachmadi@users.noreply.github.com>
2021-11-09 09:43:15 +0700

    menambahkan pagination

--- src/controllers/classes/get.classController.js
+++ src/controllers/classes/get.classController.js
@@ -1,14 +1,37 @@
-const { Class } = require("../../models");
+const {
+     Class,
+     Sequelize: { Op },
+} = require("../../models");
 
 const service = async function (req, res, next) {
      try {
           const where = {};
+          let page = Number(req.query.page) || 1;
+          let limit = Number(req.query.limit) || 3;
           if (req.params.id) {
-               where.id = req.params.id;
+               where.id = {
+                    [Op.eq]: req.params.id,
+               };
+          }
+          // const fields = ["name", "description", "createdAt", "img"];
+          // const json = {
+          //      key: "1234",
+          // };
+          // fields.forEach((field) => {
+          //      json[field] = field + "Attribute";
+          // });
+          // return res.json(json);
+          if (req.query.search) {
+               where.name = {
+                    [Op.substring]: req.query.search,
+               };
           }
           const requestDB = await Class.findAll({
-               attributes: ["id", "name", "img"],
+               // attributes: ["id", "name", "img", "createdAt"],
+               attributes: { exclude: ["updatedAt", "deletedAt"] },
                where,
+               offset: (page - 1) * limit,
+               limit,
           });
 
           if (!req.params.id) return res.json(requestDB);
